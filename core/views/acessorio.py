22436c7e7726bddffabb86704640ab65843f2696
SarahLJesus <sarahletjesus1908@gmail.com>
2025-04-29 15:54:33 -0300

    feat: criação da API para Cor.

--- core/views/acessorio.py
+++ core/views/acessorio.py
@@ -4,7 +4,6 @@ from rest_framework.viewsets import ModelViewSet
 from core.models import Acessorio
 from core.serializers import AcessorioSerializer
 
-...
 class AcessorioViewSet(ModelViewSet):
     queryset = Acessorio.objects.all()
     serializer_class = AcessorioSerializer
